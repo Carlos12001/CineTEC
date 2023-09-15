1c09a9f8890ef3e04373c53cc686abf446bdcc7f
Jose <jcur1802@gmail.com>
2023-09-14 20:40:42 -0600

    feat: api admin model controller

---
+++ api/CineTec/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using CineTec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CineTec.Controllers
+{
+    public class AdminController : ApiController
+    {
+        static Dictionary<string, Admin> admins = new Dictionary<string, Admin>();
+
+        //GET api/Admin
+        public IEnumerable<Admin> Get()
+        {
+            return new List<Admin>(admins.Values);
+        }
+
+        //GET api/Admin/pedro@cinema.com
+        public Admin Get(string Email)
+        {
+            Admin found;
+            admins.TryGetValue(Email, out found);
+            return found;
+        }
+
+        //POST api/Admin
+        public bool Post([FromBody] Admin admin)
+        {
+            Admin found;
+            admins.TryGetValue(admin.Email, out found);
+            if (found == null)
+            {
+                admins.Add(admin.Email, admin);
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        //DELETE api/Admin/pedro@cinema.com
+        public bool Delete(string Email)
+        {
+            return admins.Remove(Email);
+        }
+    }
+}
