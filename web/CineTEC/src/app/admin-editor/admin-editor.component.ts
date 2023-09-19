22d259b083d72d334d9d22c63ef6e825d8ab7d96
Carlos12001 <carlos.andres12001@gmail.com>
2023-09-18 20:12:04 -0600

    feat: add gretting admin editor

--- web/CineTEC/src/app/admin-editor/admin-editor.component.ts
+++ web/CineTEC/src/app/admin-editor/admin-editor.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Admin, admin } from '../models/admin.model';
 
 @Component({
   selector: 'app-admin-editor',
   templateUrl: './admin-editor.component.html',
   styleUrls: ['./admin-editor.component.css'],
 })
-export class AdminEditorComponent {}
+export class AdminEditorComponent implements OnInit {
+  dataAdmin: Admin | undefined;
+  ngOnInit(): void {
+    this.dataAdmin = admin;
+  }
+}
