d4fcb6d4a56e532bd016af1c0236e74e5eb7ce8d
jcur02 <jcur1802@gmail.com>
2023-09-19 21:07:23 -0600

    fix: project

---
+++ api/CineTec/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CineTec
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Habilitar CORS
+            var cors = new EnableCorsAttribute("*", "*", "*");  // Nota: No uses esto en producción, especifica los dominios permitidos.
+            config.EnableCors(cors);
+
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ api/CineTec/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CineTec.Models
+{
+    public class Movie
+    {
+        public string rating { get; set; }
+        public string director { get; set; }
+        public string duration { get; set; }
+        public string image { get; set; }
+        public string cname { get; set; }
+        public string oname { get; set; }
+        public List<string> prota { get; set; }
+
+    }
+}
