3222904c158d65c1908b3a1fa8734c114312b7b0
Carlos12001 <carlos.andres12001@gmail.com>
2023-09-19 20:31:16 -0600

    fix: project

---
+++ api/CineTec/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CineTec.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ api/CineTec/Models/Projection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CineTec.Models
+{
+    public class Projection
+    {
+        public int id { get; set; }
+        public DateTime horary { get; set; }
+        public int roomid { get; set; }
+        public string movieid { get; set; }
+    }
+}
