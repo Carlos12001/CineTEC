fcd3d185819450c93a5863135733aa5f627afe2d
Carlos12001 <carlos.andres12001@gmail.com>
2023-09-19 20:24:34 -0600

    delte

--- api/CineTec/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace CineTec.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
